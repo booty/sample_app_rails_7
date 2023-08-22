1a494a6f6fd86bebb3f1b4ff2f2a51daa64904bf
John Rose <johnedmundrose@gmail.com>
2023-08-22 01:53:23 -0400

    Add contact page; clean up routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,21 +7,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", /Home/
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", /Home/
-  end
-
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
     assert_select "title", /Help/
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", /About/
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
