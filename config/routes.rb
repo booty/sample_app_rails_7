b06eaf055e3e46d6f1cbc9a840e7bee94378c665
John Rose <johnedmundrose@gmail.com>
2023-08-23 18:03:24 -0400

    user profile prelim formatting, gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,7 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
 
-  # Defines the root path route ("/")
   root "static_pages#home"
 end
