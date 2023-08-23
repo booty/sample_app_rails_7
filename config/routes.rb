312311dd2ce9b34376e4e7e4d2bfc836594e76f5
John Rose <johnedmundrose@gmail.com>
2023-08-22 23:09:54 -0400

    Add signup link and routes, add User controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@
 
 # == Route Map
 #
-#  Prefix Verb URI Pattern        Controller#Action
-#    help GET  /help(.:format)    static_pages#help
-#   about GET  /about(.:format)   static_pages#about
-# contact GET  /contact(.:format) static_pages#contact
-#    root GET  /                  static_pages#home
+#    Prefix Verb URI Pattern          Controller#Action
+# users_new GET  /users/new(.:format) users#new
+#      help GET  /help(.:format)      static_pages#help
+#     about GET  /about(.:format)     static_pages#about
+#   contact GET  /contact(.:format)   static_pages#contact
+#      root GET  /                    static_pages#home
 
 Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
